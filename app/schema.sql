0961a2f346757a0b92d8836e8aa65801091c3cda
Granton <waribekihiko651@gmail.com>
2025-01-21 00:16:03 +0300

    rewrote schema to sqlite3 supported

--- app/schema.sql
+++ app/schema.sql
@@ -1,39 +1,39 @@
-use event_management_sys;
 CREATE TABLE IF NOT EXISTS EVENTS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    event_name VARCHAR(255) NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_name VARCHAR(50) NOT NULL,
     description TEXT,
     date DATE NOT NULL,
-    event_image VARCHAR(255),
+    event_image BLOB NULL,
     event_fee DECIMAL(10, 2) NOT NULL,
-    capacity INT NOT NULL DEFAULT 200, -- Maximum number of attendees
-    categories VARCHAR(255) DEFAULT 'General' -- Category of the event
+    capacity INTEGER NOT NULL DEFAULT 200, -- Maximum number of attendees
+    categories VARCHAR(50) DEFAULT 'General' -- Category of the event
 );
 CREATE TABLE IF NOT EXISTS CUSTOMERS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR() NOT NULL,
+    email VARCHAR() UNIQUE NOT NULL,
+    password VARCHAR() NOT NULL,
+    role VARCHAR(50) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS EVENT_MANAGERS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS EVENT_MANAGERS (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name VARCHAR() NOT NULL,
+--     email VARCHAR() UNIQUE NOT NULL,
+--     password VARCHAR() NOT NULL
+-- );
 CREATE TABLE IF NOT EXISTS PAYMENTS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    payment_method VARCHAR(255) NOT NULL,
-    event_id INT NOT NULL,
-    customer_id INT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    payment_method VARCHAR() NOT NULL,
+    event_id INTEGER NOT NULL,
+    customer_id INTEGER NOT NULL,
     amount_paid DECIMAL(10, 2) NOT NULL,
-    ratings INT CHECK (ratings BETWEEN 1 AND 5), -- Customer rating for the event
+    ratings INTEGER CHECK (ratings BETWEEN 1 AND 5), -- Customer rating for the event
     FOREIGN KEY (event_id) REFERENCES EVENTS(id),
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id)
 );
 CREATE TABLE IF NOT EXISTS NOTIFICATIONS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    event_manager_id INT NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_manager_id INTEGER NOT NULL,
     message TEXT NOT NULL,
     notification_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (event_manager_id) REFERENCES EVENT_MANAGERS(id)
@@ -45,7 +45,7 @@ CREATE TRIGGER notify_event_manager_after_payment
 AFTER INSERT ON PAYMENTS
 FOR EACH ROW
 BEGIN
-    DECLARE manager_id INT;
+    DECLARE manager_id INTEGER;
 
     -- Get the event manager for the event
     SELECT id INTO manager_id
